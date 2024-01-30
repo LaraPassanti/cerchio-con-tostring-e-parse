ba5874ac99ca1401c12f056f2061ec8ad948dea4
lara.passanti@gmail.com <lara.passanti@gmail.com>
2024-01-30 16:41:22 +0100

    Aggiungere i file di progetto.

---
+++ cerchio con tostring e parse/Cerchio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio_con_tostring_e_parse
+{
+    internal class Cerchio
+    {
+        public int raggio { get; set; }
+        public Cerchio() { }
+        public Cerchio(int r)
+        {
+            raggio = r; 
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}", raggio); 
+        }
+
+        public static Cerchio Parse(string rag) {
+            int a=int.Parse(rag);
+            Cerchio e=new Cerchio(a);
+            return e;
+        }
+    }
+}
---
+++ cerchio con tostring e parse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio_con_tostring_e_parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio(5);
+            Cerchio s = Cerchio.Parse(c.ToString());
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
